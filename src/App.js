29c20300f020f78f3ce381b512c2a82f6e6e8381
anuj-kh <f20171508@hyderabad.bits-pilani.ac.in>
2021-02-20 23:22:17 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {Helmet} from 'react-helmet';
 import './App.css';
+// import Landing from './components/landing.js'
+// import Login from './components/login.js'
+// import Signup from './components/signup.js'
+// import Dashboard from './components/dashboard.js'
 
 function App() {
   return (
-    <div className="App">
-        <h1>KHOJ</h1>
+    <>
+    <h1>KHOJ</h1>
+    <BrowserRouter>
+    <Helmet>
+    	<title> {"KHOJ - A portal for aspirants"} </title>
+    </Helmet>
+    <div>
+    	{/* <Route exact path='/' component = {Landing} />
+    	<Route exact path='/login' component = {Login} />
+    	<Route exact path='/signup' component = {Signup} />
+      <Route exact path="/dashboard" component={Dashboard}/> */}
     </div>
+    </BrowserRouter>
+    </>
   );
 }
 
