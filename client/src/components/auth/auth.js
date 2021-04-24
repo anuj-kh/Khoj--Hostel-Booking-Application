72b482a6aca4678806ca1216deab2107389337da
Saksham Gupta <saksham1319@gmail.com>
2021-04-24 11:22:59 +0530

    bracket miss

--- client/src/components/auth/auth.js
+++ client/src/components/auth/auth.js
@@ -53,196 +53,197 @@ const Auth = () => {
 
 
   const handleSubmit = (e) => {
-    setSuccess("");
-    setError("");
-    e.preventDefault();
-    console.log(form);
-    if (isSignup) 
-    {
-        if(form.phone.length!=10)
-            setError("Phone number should be of size 10!!");
-        else
+        setSuccess("");
+        setError("");
+        e.preventDefault();
+        console.log(form);
+        if (isSignup) 
         {
-            if(form.password==form.confirmPassword)
-            {
-                (async () => {
-                const a = await dispatch(signup(form, history));
-                console.log(a);
-                if(a!=null)
-                    setError(a);
-                else  
-                {
-                    setSuccess("Succesfully registered!!");
-                    setIsSignup((prevIsSignup) => !prevIsSignup);
-                }
-                })();
-            }
+            if(form.phone.length!=10)
+                setError("Phone number should be of size 10!!");
             else
-                setError("The passwords don't match!!");
-        }
-    } 
-    else 
-    {
-      (async () => {
-        const a = await dispatch(signin(form, history));
-        console.log(a);
-        if(a!=null)
-          setError(a);
-      })();
-
-    }
-    const googleSuccess = async (res) => {
-        const result = res?.profileObj;
-
+            {
+                if(form.password==form.confirmPassword)
+                {
+                    (async () => {
+                    const a = await dispatch(signup(form, history));
+                    console.log(a);
+                    if(a!=null)
+                        setError(a);
+                    else  
+                    {
+                        setSuccess("Succesfully registered!!");
+                        setIsSignup((prevIsSignup) => !prevIsSignup);
+                    }
+                    })();
+                }
+                else
+                    setError("The passwords don't match!!");
+            }
+        } 
+        else 
+        {
         (async () => {
+            const a = await dispatch(signin(form, history));
+            console.log(a);
+            if(a!=null)
+            setError(a);
+        })();
 
-            const a = await dispatch(gSignin(result, history))
-            console.log(a)
-            if (a != null) setError(a)
-        })()
-    }
-    const googleError = () =>
-        alert('Google Sign In was unsuccessful. Try again later')
+        }
+        const googleSuccess = async (res) => {
+            const result = res?.profileObj;
 
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-        console.log(form)
-    }
+            (async () => {
 
-    return (
-        <Container component='main' maxWidth='xs'>
-            <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component='h1' variant='h5'>
-                    {isSignup ? 'Sign up' : 'Sign in'}
-                </Typography>
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <div className={classes.successDiv}>{success}</div>
-                    <Grid container spacing={2}>
-                        {isSignup && (
-                            <>
-                                <Input
-                                    name='firstName'
-                                    label='First Name'
-                                    handleChange={handleChange}
-                                    autoFocus
-                                    half
-                                />
-                                <Input
-                                    name='lastName'
-                                    label='Last Name'
-                                    handleChange={handleChange}
-                                    half
-                                />
-                                <Input
-                                    name='email'
-                                    label='Email Address'
-                                    handleChange={handleChange}
-                                    type='email'
-                                />
-                                <Input
-                                    name='phone'
-                                    label='Phone number'
-                                    handleChange={handleChange}
-                                    type='number'
-                                />
-                                <Input
-                                    name='password'
-                                    label='Password'
-                                    handleChange={handleChange}
-                                    type={showPassword ? 'text' : 'password'}
-                                    handleShowPassword={handleShowPassword}
-                                />
-                                <Input
-                                    name='confirmPassword'
-                                    label='Repeat Password'
-                                    handleChange={handleChange}
-                                    type={showPassword ? 'text' : 'password'}
-                                />
+                const a = await dispatch(gSignin(result, history))
+                console.log(a)
+                if (a != null) setError(a)
+            })()
+        }
+        const googleError = () =>
+            alert('Google Sign In was unsuccessful. Try again later')
 
-                                <FormControl
-                                    className={classes.formControl}
-                                    required>
-                                    <InputLabel>Type of user</InputLabel>
-                                    <Select
-                                        name='user'
-                                        value={form.user}
+        const handleChange = (e) => {
+            setForm({ ...form, [e.target.name]: e.target.value })
+            console.log(form)
+        }
+
+        return (
+            <Container component='main' maxWidth='xs'>
+                <Paper className={classes.paper} elevation={3}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component='h1' variant='h5'>
+                        {isSignup ? 'Sign up' : 'Sign in'}
+                    </Typography>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        <div className={classes.successDiv}>{success}</div>
+                        <Grid container spacing={2}>
+                            {isSignup && (
+                                <>
+                                    <Input
+                                        name='firstName'
+                                        label='First Name'
+                                        handleChange={handleChange}
+                                        autoFocus
+                                        half
+                                    />
+                                    <Input
+                                        name='lastName'
+                                        label='Last Name'
+                                        handleChange={handleChange}
+                                        half
+                                    />
+                                    <Input
+                                        name='email'
+                                        label='Email Address'
+                                        handleChange={handleChange}
+                                        type='email'
+                                    />
+                                    <Input
+                                        name='phone'
+                                        label='Phone number'
+                                        handleChange={handleChange}
+                                        type='number'
+                                    />
+                                    <Input
+                                        name='password'
+                                        label='Password'
+                                        handleChange={handleChange}
+                                        type={showPassword ? 'text' : 'password'}
+                                        handleShowPassword={handleShowPassword}
+                                    />
+                                    <Input
+                                        name='confirmPassword'
+                                        label='Repeat Password'
+                                        handleChange={handleChange}
+                                        type={showPassword ? 'text' : 'password'}
+                                    />
+
+                                    <FormControl
                                         className={classes.formControl}
-                                        onChange={handleChange}>
-                                        <MenuItem value={'Student'}>
-                                            Student
-                                        </MenuItem>
-                                        <MenuItem value={'Service Provider'}>
-                                            Service Provider
-                                        </MenuItem>
-                                        <MenuItem value={'Admin'}>
-                                            Admin
-                                        </MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </>
-                        )}
-                        {!isSignup && (
-                            <>
-                                <Input
-                                    name='email'
-                                    label='Email Address'
-                                    handleChange={handleChange}
-                                    type='email'
-                                    autoFocus
-                                />
-                                <Input
-                                    name='password'
-                                    label='Password'
-                                    handleChange={handleChange}
-                                    type={showPassword ? 'text' : 'password'}
-                                    handleShowPassword={handleShowPassword}
-                                />
-                            </>
-                        )}
-                    </Grid>
-                    <div className={classes.errorDiv}>{error}</div>
-                    <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        color='primary'
-                        className={classes.submit}>
-                        {isSignup ? 'Sign Up' : 'Sign In'}
-                    </Button>
-                    <GoogleLogin
-                        clientId='868546255367-a9e3v26e7eaig3hleuukheu6d72kqdge.apps.googleusercontent.com'
-                        render={(renderProps) => (
-                            <Button
-                                className={classes.googleButton}
-                                color='primary'
-                                fullWidth
-                                onClick={renderProps.onClick}
-                                disabled={renderProps.disabled}
-                                startIcon={<Icon />}
-                                variant='contained'>
-                                Google Sign In
-                            </Button>
-                        )}
-                        onSuccess={googleSuccess}
-                        onFailure={googleError}
-                        cookiePolicy='single_host_origin'
-                    />
-                    <Grid container justify='flex-end'>
-                        <Grid item>
-                            <Button onClick={switchMode}>
-                                {isSignup
-                                    ? 'Already have an account? Sign in'
-                                    : "Don't have an account? Sign Up"}
-                            </Button>
+                                        required>
+                                        <InputLabel>Type of user</InputLabel>
+                                        <Select
+                                            name='user'
+                                            value={form.user}
+                                            className={classes.formControl}
+                                            onChange={handleChange}>
+                                            <MenuItem value={'Student'}>
+                                                Student
+                                            </MenuItem>
+                                            <MenuItem value={'Service Provider'}>
+                                                Service Provider
+                                            </MenuItem>
+                                            <MenuItem value={'Admin'}>
+                                                Admin
+                                            </MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </>
+                            )}
+                            {!isSignup && (
+                                <>
+                                    <Input
+                                        name='email'
+                                        label='Email Address'
+                                        handleChange={handleChange}
+                                        type='email'
+                                        autoFocus
+                                    />
+                                    <Input
+                                        name='password'
+                                        label='Password'
+                                        handleChange={handleChange}
+                                        type={showPassword ? 'text' : 'password'}
+                                        handleShowPassword={handleShowPassword}
+                                    />
+                                </>
+                            )}
                         </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-        </Container>
-    )
+                        <div className={classes.errorDiv}>{error}</div>
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            color='primary'
+                            className={classes.submit}>
+                            {isSignup ? 'Sign Up' : 'Sign In'}
+                        </Button>
+                        <GoogleLogin
+                            clientId='868546255367-a9e3v26e7eaig3hleuukheu6d72kqdge.apps.googleusercontent.com'
+                            render={(renderProps) => (
+                                <Button
+                                    className={classes.googleButton}
+                                    color='primary'
+                                    fullWidth
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}
+                                    startIcon={<Icon />}
+                                    variant='contained'>
+                                    Google Sign In
+                                </Button>
+                            )}
+                            onSuccess={googleSuccess}
+                            onFailure={googleError}
+                            cookiePolicy='single_host_origin'
+                        />
+                        <Grid container justify='flex-end'>
+                            <Grid item>
+                                <Button onClick={switchMode}>
+                                    {isSignup
+                                        ? 'Already have an account? Sign in'
+                                        : "Don't have an account? Sign Up"}
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+            </Container>
+        )
+  }
 }
 
 export default Auth
