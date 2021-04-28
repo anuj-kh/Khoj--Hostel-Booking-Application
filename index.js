3cf135a443bc79e70236125c107afce5efceaa3e
Anuj Kharbanda <f20171508@hyderabad.bits-pilani.ac.in>
2021-04-28 20:40:50 +0530

    static path

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 // Set EJS as templating engine
+
+if (process.env.NODE_ENV ==='production')
+{
+    app.use(express.static('client/build'));
+    const path=require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.set('view engine', 'ejs')
 app.use('/user', userRouter)
 app.use('/dashboard', userDashboard)
@@ -23,16 +33,6 @@ mongoose
     .connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    })
-    .then( () => {
-        if (process.env.NODE_ENV ==='production')
-        {
-            app.use(express.static('client/build'));
-            const path=require('path')
-            app.get("*",(req,res)=>{
-                res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-            })
-        }
     })
     .then(() =>
         app.listen(PORT, () =>
