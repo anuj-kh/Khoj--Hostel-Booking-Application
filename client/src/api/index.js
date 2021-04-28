d69a83391b2bc4094dc92ab73bc33bf290d6b536
Anuj Kharbanda <f20171508@hyderabad.bits-pilani.ac.in>
2021-04-28 18:51:06 +0530

    more changes 2

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+// const API = axios.create({ baseURL: 'http://localhost:5000' })
 
-API.interceptors.request.use((req) => {
+axios.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${
             JSON.parse(localStorage.getItem('profile')).token
@@ -13,14 +13,14 @@ API.interceptors.request.use((req) => {
 })
 
 export const signIn = (formData) =>
-    API.post('/user/signin', formData).then((res) => {
+    axios.post('/user/signin', formData).then((res) => {
         return res
     })
 export const signUp = (formData) =>
-    API.post('/user/signup', formData).then((res) => {
+    axios.post('/user/signup', formData).then((res) => {
         return res
     })
 export const gSignIn = (formData) =>
-    API.post('/user/gSignin', formData).then((res) => {
+    axios.post('/user/gSignin', formData).then((res) => {
         return res
     })
